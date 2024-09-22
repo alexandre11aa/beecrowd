c29956ff7cf399a33e0ac2dd652e31311c3ec4e5
Alexandre Estrela <82901702+alexandre11aa@users.noreply.github.com>
2024-09-21 23:08:22 -0300

    1006.cpp

--- c++/1006.cpp
+++ c++/1006.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
  
 int main() {
@@ -12,19 +11,4 @@ int main() {
     printf("MEDIA = %.1lf\n", ((A * 2 + B * 3 + C * 5) / 10));
  
     return 0;
-=======
-#include <stdio.h>
- 
-int main() {
- 
-    double A, B, C;
-
-    scanf("%lf", & A);
-    scanf("%lf", & B);
-    scanf("%lf", & C);
-
-    printf("MEDIA = %.1lf\n", ((A * 2 + B * 3 + C * 5) / 10));
- 
-    return 0;
->>>>>>> 1cfb7856b5cd16d06c82bd088e56144c1fd011c8
 }
