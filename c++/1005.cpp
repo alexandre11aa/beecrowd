be80cf75b9f719dc9ba3d0c3385ce63d1c3d6c2d
Alexandre Estrela <82901702+alexandre11aa@users.noreply.github.com>
2024-09-21 23:08:07 -0300

    1005.cpp

--- c++/1005.cpp
+++ c++/1005.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
  
 int main() {
@@ -11,18 +10,4 @@ int main() {
     printf("MEDIA = %.5lf\n", ((A * 3.5 + B * 7.5) / 11));
  
     return 0;
-=======
-#include <stdio.h>
- 
-int main() {
- 
-    double A, B;
-
-    scanf("%lf", & A);
-    scanf("%lf", & B);
-
-    printf("MEDIA = %.5lf\n", ((A * 3.5 + B * 7.5) / 11));
- 
-    return 0;
->>>>>>> 1cfb7856b5cd16d06c82bd088e56144c1fd011c8
 }
